9e2fb962194bfdbc1d6e4557c6647f8cd159e594
Michel Chung <michel@vitavinum.com>
2019-12-13 11:42:09 +0100

    Create vue component GoogleMaps.vue

---
+++ src/components/GoogleMaps.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="google-map"></div>
+</template>
+
+<script>
+export default {
+    name: 'GoogleMaps'
+}
+</script>
+
+<style>
+#google-map {
+    height: 100%;
+    width: 100%;
+}
+</style>
